3325a86e148c2f516b2ad1ce813c9b1a4104936d
Bbromw <kaystim6@gmail.com>
2022-07-26 11:35:50 +0700

    chang request to https

--- src/api/api.js
+++ src/api/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseURL = `http://www.omdbapi.com/?&apikey=${process.env.REACT_APP_API_KEY}`;
+const baseURL = `https://www.omdbapi.com/?&apikey=${process.env.REACT_APP_API_KEY}`;
 export const getMovies = (title) => {
   return axios.get(`${baseURL}&s=${title}&type=movie`);
 };
