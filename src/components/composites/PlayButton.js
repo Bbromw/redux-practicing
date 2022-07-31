e5e7ec2c4c93d92a28fedd91676146d6f4a06709
Nguyễn Hữu Phúc <phuc82908@gmail.com>
2022-07-31 17:14:14 +0700

    Refactor UI for Movie details page.

---
+++ src/components/composites/PlayButton.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const PlayButton = () => {
+  return (
+    <a id="play-video" class="video-play-button" href="/">
+      <span></span>
+    </a>
+  )
+}
+
+export default PlayButton
