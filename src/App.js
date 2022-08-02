37a624adb5efb703bb9c39efd1e337e46ea26d92
Bbromw <kaystim6@gmail.com>
2022-08-02 20:26:02 +0700

    use images have been in project

--- src/App.js
+++ src/App.js
@@ -64,7 +64,7 @@ function App() {
           <DetailsPage />
         </Route>
       </Switch>
-      <Footer />
+      {/* <Footer /> */}
     </div>
   );
 }
--- src/components/movie-detail/N_movie_detail/NMovieDetail.js
+++ src/components/movie-detail/N_movie_detail/NMovieDetail.js
@@ -1,27 +1,35 @@
 import React, { useEffect, useRef } from "react";
 import "./NMovieDetail.scss";
-import moviePicture from "../../../images/wallpapersden.com_cowboy-bebop-hd-netflix-poster_2048x1152.jpg";
+import moviePicture from "../../../images/t1.jpg";
 import { useSelector, useDispatch } from "react-redux/es/exports";
 import { movieDetailAction } from "../../../store";
 import { useParams } from "react-router-dom";
-import sense1 from "../../../images/8dfcf4f7c5864b885e2993a0ef32359adc323c36.jpg";
-import sense2 from "../../../images/review-cowboy-bebop-live-action-2_XSNW.webp";
+import sense1 from "../../../images/t1.jpg";
+import sense2 from "../../../images/t2.jpg";
+import sense3 from "../../../images/t3.jpg";
+import sense4 from "../../../images/t4.jpg";
 
 const NMovieDetail = () => {
   const { movie } = useSelector((state) => state.movieDetailReducer);
 
-  const { movies } = useSelector((state) => state.moviesReducer);
   const dispatch = useDispatch();
   const { id } = useParams();
 
   const moviesElement = useRef(null);
 
-  console.log(movie);
+  const senseElement = useRef(null)
 
   const nextFilm = () => {
     const movies = moviesElement.current;
     movies.append(movies.children[0]);
   };
+
+  const nextSense = () => {
+    const senses = senseElement.current;
+    senses.append(senses.children[0]);
+  }
+
+
 
   useEffect(() => {
     dispatch(movieDetailAction.getMovieDetailThunk(id));
@@ -34,7 +42,7 @@ const NMovieDetail = () => {
       <div className="overlay"></div>
       <div className="left-side">
         <div className="heading">
-          <h1 className="title">{movie.Title}</h1>
+          <h1 className="title">TRANSFORMERS: THE LAST KNIGHT</h1>
           <div className="extra-heading">
             <div className="item rating">{movie.imdbRating} &#183;</div>
             <div className="item runtime">{movie.Runtime} &#183;</div>
@@ -89,28 +97,36 @@ const NMovieDetail = () => {
             <i className="icon fa fa-plus-circle" aria-hidden="true"></i>
           </div>
         </div>
-        <div className="other-movies">
+        <div className="other-movies" >
           <div className="heading">Senses in film</div>
-          <div className="movies" ref={moviesElement}>
-            <div className="movie" key={movie.imdbID}>
-              <img src={sense1} />
-            </div>
+          <div className="movies" ref={senseElement}>
             <div className="movie" key={movie.imdbID}>
               <img src={sense2} />
             </div>
+            <div className="movie" key={movie.imdbID}>
+              <img src={sense3} />
+            </div>
           </div>
-          <button className="next-button" onClick={nextFilm}>
+          <button className="next-button" onClick={nextSense}>
             Next &#62;
           </button>
         </div>
         <div className="other-movies">
           <div className="heading">Related Movies</div>
           <div className="movies" ref={moviesElement}>
-            {movies.map((movie) => (
-              <div className="movie" key={movie.imdbID}>
-                <img src={movie.Poster} />
-              </div>
-            ))}
+            {/* {pathImg.map((path, index) => {console.log(path)})} */}
+            <div className="movie" key={movie.imdbID}>
+              <img src={sense1} />
+            </div>
+            <div className="movie" key={movie.imdbID}>
+              <img src={sense2} />
+            </div>
+            <div className="movie" key={movie.imdbID}>
+              <img src={sense3} />
+            </div>
+            <div className="movie" key={movie.imdbID}>
+              <img src={sense4} />
+            </div>
           </div>
           <button className="next-button" onClick={nextFilm}>
             Next &#62;
