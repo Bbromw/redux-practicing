7523fe9b7fb28ab60e43c7534230db3b8526ffb7
ext-phuc-nguyen <ext-phuc.nguyen@spdigital.sg>
2022-08-03 09:48:43 +0700

    Fix up the navigation of movie app

--- src/App.js
+++ src/App.js
@@ -1,34 +1,20 @@
-import "./App.scss";
-import { MoviesPage } from "./components/movies";
-import { Switch, Route, Link } from "react-router-dom";
-import { Footer } from "./components/footer";
-import { useRef } from "react";
-import DetailsPage from "./components/movie-detail/DetailsPage";
-import NMovieDetail from "./components/movie-detail/N_movie_detail/NMovieDetail";
+import "./App.scss"
+import { MoviesPage } from "./components/movies"
+import { Switch, Route, Link } from "react-router-dom"
+import { Footer } from "./components/footer"
+import { useRef } from "react"
+import DetailsPage from "./components/movie-detail/DetailsPage"
+import NMovieDetail from "./components/movie-detail/N_movie_detail/NMovieDetail"
 
 function App() {
-  const menuList = useRef(null);
-
-  const clickMenu = () => {
-    const list = menuList;
-    list.current.style.marginLeft = 0;
-  };
-
-  const CloseMenu = () => {
-    const list = menuList;
-    list.current.style.marginLeft = "-100%";
-  };
   return (
     <div className="App">
       <nav className="nav">
-        <div className="nav__menu">
-          <div className="nav__menu__icon" onClick={clickMenu}>
-            <i className="fa fa-bars" aria-hidden="true"></i>
-          </div>
-          <ul className="nav__menu__list" ref={menuList}>
-            <li className="nav__menu__list__close-icon" onClick={CloseMenu}>
-              <i className="fa fa-times-circle" aria-hidden="true"></i>
-            </li>
+        <div className="logo">
+          <div className="logo__brand">MOZI</div>
+        </div>
+        <div className="buttons">
+          <ul className="buttons buttons-inside">
             <li>
               <Link to="/">Movies</Link>
             </li>
@@ -38,19 +24,9 @@ function App() {
             <li>
               <Link to="/">Help Center</Link>
             </li>
-            <div className="buttons buttons-inside">
-              <button className="sign-in-button">Sign In</button>
-              <button className="premium-button">Go Premium</button>
-            </div>
           </ul>
-        </div>
-        <div className="logo">
-          <div className="logo__brand">mozi</div>
-          <div className="logo__extra">stream</div>
-        </div>
-        <div className="buttons">
-          <button className="sign-in-button">Sign In</button>
-          <button className="premium-button">Go Premium</button>
+          <button className="sign-in-btn">Sign In</button>
+          <button className="premium-btn">Go Premium</button>
         </div>
       </nav>
       <Switch>
@@ -66,7 +42,7 @@ function App() {
       </Switch>
       {/* <Footer /> */}
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
